4d9f0a46b950e153576d052a64c2fbc976462a82
rde9 <41101885+rde9@users.noreply.github.com>
2025-03-25 17:34:17 +0800

    fix: remove unnecessary heading mappings

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -135,11 +135,11 @@ const NodesRenderer: FC<{ nodes: RootContent[] }> = ({ nodes }) => {
 const HeadingNode: FC<{ node: RootContentMap['heading'] }> = ({ node }) => {
   const Component = (
     {
-      1: 'h2',
-      2: 'h3',
-      3: 'h4',
-      4: 'h5',
-      5: 'h6',
+      1: 'h1',
+      2: 'h2',
+      3: 'h3',
+      4: 'h4',
+      5: 'h5',
       6: 'h6',
     } as const
   )[node.depth];
