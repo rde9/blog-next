570f5943630f796c30e3914b578bd61ad7fb7911
rde9 <41101885+rde9@users.noreply.github.com>
2025-06-10 09:37:32 +0800

    feat: sprite logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,15 +12,13 @@ const Header: FC = () => {
   return (
     <div id='header-wrapper' className='animate-bg-img md:animate-none'>
       <header className='header-container grid grid-cols-1 items-center justify-items-center md:grid-cols-[auto_1fr] md:justify-items-start'>
-        <div className='md:animate-header-left'>
+        <div className='logo-container md:animate-header-left'>
           <Link
             className='mt-4 grid grid-cols-[auto_1fr] items-center text-xl md:mt-2'
             href='/'
           >
             <Logo />
-            <span className='relative z-10 px-1 font-fredoka text-2xl font-medium text-link transition-colors duration-500 before:absolute before:inset-0 before:-z-10 before:my-auto before:origin-top-right before:scale-x-0 before:bg-link before:transition-transform before:duration-500 hover:text-gray-50 hover:before:origin-top-left hover:before:scale-x-100 dark:text-primary-text dark:before:bg-primary-text hover:dark:text-primary-600'>
-              {SITE_TITLE}
-            </span>
+            <span className='logo-text'>{SITE_TITLE}</span>
           </Link>
         </div>
         <div className='md:animate-header-right md:justify-self-end'>
@@ -85,7 +83,7 @@ const NavbarItem: FC<NavItemProps> = ({ href, item }) => {
 function Logo() {
   return (
     <div
-      className='logo-wrapper -mr-2 -mt-4 h-16 w-16 flex-shrink-0 bg-contain bg-center bg-no-repeat'
+      className='logo-wrapper -mr-2 -mt-4 h-16 w-16 bg-cover bg-no-repeat'
       aria-label='Logo'
     />
   );
